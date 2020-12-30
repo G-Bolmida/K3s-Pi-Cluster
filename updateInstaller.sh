3bd8e3053b322b6648da9529451019de5248911b
George Bolmida <gbolmida@georgebolmida.com>
2020-12-30 13:18:02 -0500

    Fixed K3s script download and removed workaround script

--- updateInstaller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Author: George Bolmida
-# Downloads the most recent K3s install script to the directory
-
-curl -sfL https://get.k3s.io > k3sInstall.sh
-echo "Script written to 'k3sInstall.sh'"
